51b67e4440ae53b6bc4f3a02714d7ed9b6b3b3f4
Dzianis Haurylavets <dzianis.haurylavets-ext@ax-ao.de>
2018-12-21 14:44:58 +0300

    Add LED controller

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const bot = new TeleBot({
     token: '742600804:AAHVflkpctW6p1fRS6RVuLxlFqWY6IB9kuM'
 });
 
+const Gpio = require('onoff').Gpio;
+const LED = new Gpio(4, 'out');
+
 const replyMarkup = bot.keyboard([
     ['/TurnOnLights'],
     ['/TurnOffLights']
 ], {resize: true});
+
 
 bot.on(['/start', '/back', '/help'], msg => {
     console.log('User ' + msg.from.username + " started conversation");
@@ -15,11 +19,33 @@ bot.on(['/start', '/back', '/help'], msg => {
 
 bot.on(['/TurnOnLights'], msg => {
     console.log('User ' + msg.from.username + " sent message " + msg.text);
-    return bot.sendMessage(msg.from.id, 'You switched on Xmas three', {replyMarkup});
+    LED.write(1, (err, resp) => {
+        if (err) {
+            bot.sendMessage(msg.from.id, 'Error while switching on lights', {replyMarkup});
+            console.error(err);
+            return;
+        }
+        bot.sendMessage(msg.from.id, 'You switched on Xmas three', {replyMarkup});
+    });
 });
+
 bot.on(['/TurnOffLights'], msg => {
     console.log('User ' + msg.from.username + " sent message " + msg.text);
-    return bot.sendMessage(msg.from.id, 'You switched off Xmas three', {replyMarkup});
+    LED.write(0, (err, resp) => {
+        if (err) {
+            bot.sendMessage(msg.from.id, 'Error while switching off lights', {replyMarkup});
+            console.error(err);
+            return;
+        }
+        bot.sendMessage(msg.from.id, 'You switched off Xmas three', {replyMarkup});
+    });
 });
+
+
+process.on('SIGINT', () => {
+    LED.writeSync(0);
+    LED.unexport();
+    pushButton.unexport();
+}); //function to run when user closes using ctrl+c
 
 bot.start();
