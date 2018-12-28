70445bb119f460e777e0967b8b63dc98fd51baed
Dzianis Haurylavets <dzianis.haurylavets-ext@ax-ao.de>
2018-12-28 12:57:48 +0300

    Add blinking

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ const Gpio = require('onoff').Gpio;
 const LED1 = new Gpio(17, 'out');
 const LED2 = new Gpio(16, 'out');
 
-const replyMarkup = bot.keyboard([
-    ['/TurnOnLights'],
-    ['/TurnOffLights']
-], {resize: true});
+const replyMarkup = bot.keyboard([['/TurnOn', '/TurnOff', '/Blink'], ['/Status']], {resize: true});
 
+let state = 'off';
 
-bot.on(['/start', '/back', '/help'], msg => {
+bot.on(msg => {
     console.log('User ' + msg.from.username + " started conversation");
     return bot.sendMessage(msg.from.id, 'You can switch Xmas three here', {replyMarkup});
 });
@@ -30,8 +28,12 @@ function logMessage(msg) {
     console.log(txt);
 }
 
-bot.on(['/TurnOnLights'], msg => {
+bot.on(['/TurnOn'], msg => {
     logMessage(msg);
+    if (state === 'on') {
+        bot.sendMessage(msg.from.id, 'LEDs are already turned on', {replyMarkup});
+        return;
+    }
     LED1.write(0, err => {
         if (err) {
             console.error(err);
@@ -44,12 +46,18 @@ bot.on(['/TurnOnLights'], msg => {
                 return;
             }
             console.log("LED2 turned on");
+            state = 'on';
             bot.sendMessage(msg.from.id, 'You switched on Xmas tree', {replyMarkup});
         })
     });
 });
-bot.on(['/TurnOffLights'], msg => {
+
+bot.on(['/TurnOff'], msg => {
     logMessage(msg);
+    if (state === 'off') {
+        bot.sendMessage(msg.from.id, 'LEDs are already turned off', {replyMarkup});
+        return;
+    }
     LED1.write(1, err => {
         if (err) {
             console.error(err);
@@ -62,9 +70,51 @@ bot.on(['/TurnOffLights'], msg => {
                 return;
             }
             console.log("LED2 turned off");
+            state = 'off';
             bot.sendMessage(msg.from.id, 'You switched off Xmas tree', {replyMarkup});
         })
     });
+});
+
+bot.on(['/Status'], msg => {
+    logMessage(msg);
+    bot.sendMessage(msg.from.id, 'LEDs are ' + state + ' now', {replyMarkup});
+});
+
+bot.on(['/Blink'], msg => {
+    logMessage(msg);
+    if (state === 'blink') {
+        bot.sendMessage(msg.from.id, 'LEDs are already blinking', {replyMarkup});
+        return;
+    }
+
+    let state1 = state;
+    let state2 = state;
+
+    const ledBlink1 = setInterval(() => {
+        if (state !== 'blink') {
+            clearTimeout(ledBlink1);
+            return;
+        }
+        if (state1 === 'on') {
+            LED1.write(1, () => state1 = 'off');
+        } else {
+            LED1.write(0, () => state1 = 'on');
+        }
+    }, 1000);
+    const ledBlink2 = setInterval(() => {
+        if (state !== 'blink') {
+            clearTimeout(ledBlink1);
+            return;
+        }
+        if (state2 === 'on') {
+            LED2.write(1, () => state2 = 'off');
+        } else {
+            LED2.write(0, () => state2 = 'on');
+        }
+    }, 618);
+    state = 'blink';
+    bot.sendMessage(msg.from.id, 'You blink Xmas tree', {replyMarkup});
 });
 
 process.on('SIGINT', () => {
