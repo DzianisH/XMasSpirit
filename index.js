9dc72ca539c31313ed4837f6708068f983aa0efd
Dzianis Haurylavets <dzianis.haurylavets-ext@ax-ao.de>
2018-12-28 14:23:11 +0300

    tabs!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot({
-    token: '742600804:AAHVflkpctW6p1fRS6RVuLxlFqWY6IB9kuM'
+	token: '742600804:AAHVflkpctW6p1fRS6RVuLxlFqWY6IB9kuM'
 });
 
 const Gpio = require('onoff').Gpio;
@@ -16,122 +16,122 @@ const replyMarkup = bot.keyboard(
 let state = 'off';
 
 function logMessage(msg) {
-    let txt;
-    if (msg.from.username) {
-        txt = 'User ' + msg.from.first_name + " " + msg.from.last_name + " " + msg.from.username + " sent message " + msg.text;
-    } else {
-        txt = 'User ' + msg.from.first_name + " " + msg.from.last_name + " sent message " + msg.text;
-    }
+	let txt;
+	if (msg.from.username) {
+		txt = 'User ' + msg.from.first_name + " " + msg.from.last_name + " " + msg.from.username + " sent message " + msg.text;
+	} else {
+		txt = 'User ' + msg.from.first_name + " " + msg.from.last_name + " sent message " + msg.text;
+	}
 
-    console.log(txt);
+	console.log(txt);
 }
 
 bot.on(['/TurnOn'], msg => {
-    msg.processed = true;
-    logMessage(msg);
-    if (state === 'on') {
-        bot.sendMessage(msg.from.id, 'LEDs are already turned on', {replyMarkup});
-        return;
-    }
-    LED1.write(0, err => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log("LED1 turned on");
-        LED2.write(0, err => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log("LED2 turned on");
-            state = 'on';
-            bot.sendMessage(msg.from.id, 'You switched on Xmas tree', {replyMarkup});
-        })
-    });
+	msg.processed = true;
+	logMessage(msg);
+	if (state === 'on') {
+		bot.sendMessage(msg.from.id, 'LEDs are already turned on', {replyMarkup});
+		return;
+	}
+	LED1.write(0, err => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log("LED1 turned on");
+		LED2.write(0, err => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log("LED2 turned on");
+			state = 'on';
+			bot.sendMessage(msg.from.id, 'You switched on Xmas tree', {replyMarkup});
+		})
+	});
 });
 
 bot.on(['/TurnOff'], msg => {
-    msg.processed = true;
-    logMessage(msg);
-    if (state === 'off') {
-        bot.sendMessage(msg.from.id, 'LEDs are already turned off', {replyMarkup});
-        return;
-    }
-    LED1.write(1, err => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log("LED1 turned off");
-        LED2.write(1, err => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log("LED2 turned off");
-            state = 'off';
-            bot.sendMessage(msg.from.id, 'You switched off Xmas tree', {replyMarkup});
-        })
-    });
+	msg.processed = true;
+	logMessage(msg);
+	if (state === 'off') {
+		bot.sendMessage(msg.from.id, 'LEDs are already turned off', {replyMarkup});
+		return;
+	}
+	LED1.write(1, err => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log("LED1 turned off");
+		LED2.write(1, err => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log("LED2 turned off");
+			state = 'off';
+			bot.sendMessage(msg.from.id, 'You switched off Xmas tree', {replyMarkup});
+		})
+	});
 });
 
 bot.on(['/Status'], msg => {
-    msg.processed = true;
-    logMessage(msg);
-    bot.sendMessage(msg.from.id, 'LEDs are ' + state + ' now', {replyMarkup});
+	msg.processed = true;
+	logMessage(msg);
+	bot.sendMessage(msg.from.id, 'LEDs are ' + state + ' now', {replyMarkup});
 });
 
 bot.on(['/Blink'], msg => {
-    msg.processed = true;
-    logMessage(msg);
-    if (state === 'blink') {
-        bot.sendMessage(msg.from.id, 'LEDs are already blinking', {replyMarkup});
-        return;
-    }
+	msg.processed = true;
+	logMessage(msg);
+	if (state === 'blink') {
+		bot.sendMessage(msg.from.id, 'LEDs are already blinking', {replyMarkup});
+		return;
+	}
 
-    let state1 = state;
-    let state2 = state;
+	let state1 = state;
+	let state2 = state;
 
-    const ledBlink1 = setInterval(() => {
-        if (state !== 'blink') {
-            clearTimeout(ledBlink1);
-            return;
-        }
-        if (state1 === 'on') {
-            LED1.write(1, () => state1 = 'off');
-        } else {
-            LED1.write(0, () => state1 = 'on');
-        }
-    }, 1000);
-    const ledBlink2 = setInterval(() => {
-        if (state !== 'blink') {
-            clearTimeout(ledBlink1);
-            return;
-        }
-        if (state2 === 'on') {
-            LED2.write(1, () => state2 = 'off');
-        } else {
-            LED2.write(0, () => state2 = 'on');
-        }
-    }, 618);
-    state = 'blink';
-    bot.sendMessage(msg.from.id, 'You blink Xmas tree', {replyMarkup});
+	const ledBlink1 = setInterval(() => {
+		if (state !== 'blink') {
+			clearTimeout(ledBlink1);
+			return;
+		}
+		if (state1 === 'on') {
+			LED1.write(1, () => state1 = 'off');
+		} else {
+			LED1.write(0, () => state1 = 'on');
+		}
+	}, 1000);
+	const ledBlink2 = setInterval(() => {
+		if (state !== 'blink') {
+			clearTimeout(ledBlink1);
+			return;
+		}
+		if (state2 === 'on') {
+			LED2.write(1, () => state2 = 'off');
+		} else {
+			LED2.write(0, () => state2 = 'on');
+		}
+	}, 618);
+	state = 'blink';
+	bot.sendMessage(msg.from.id, 'You blink Xmas tree', {replyMarkup});
 });
 
 
 bot.on(['text'], msg => {
-    if(!msg.processed) {
-        logMessage(msg);
-        bot.sendMessage(msg.from.id, 'You can switch Xmas three here', {replyMarkup});
-    }
+	if (!msg.processed) {
+		logMessage(msg);
+		bot.sendMessage(msg.from.id, 'You can switch Xmas three here', {replyMarkup});
+	}
 });
 
 process.on('SIGINT', () => {
-    LED1.write(0, () => {
-        LED1.unexport();
-        LED2.write(0, () => LED2.unexport());
-    });
+	LED1.write(0, () => {
+		LED1.unexport();
+		LED2.write(0, () => LED2.unexport());
+	});
 }); //function to run when user closes using ctrl+c
 
 bot.start();
